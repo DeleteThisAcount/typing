2620ef561793d93a180ddaf1b6c7addd8e375325
BookOwl <stanleybookowl@gmail.com>
2017-07-03 10:40:31 -0400

    Save the user input to localStorage on change. Now the user can work on and view their notes after the reload the website.

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ window.onload = (function() {
     console.log("onload")
     var clearedTextBox = 0;
     var text_box = document.getElementById("textbox");
-    text_box.onclick = (function() {
-        console.log("onclick")
-        if (clearedTextBox === 0) {
-            text_box.innerHTML = "";
-            clearedTextBox = 1
-        }
-    });
+    var input = localStorage.getItem("input");
+    if (input) {
+        text_box.innerHTML = input;
+    } else {
+        text_box.innerHTML = "Type here!";
+        text_box.onclick = function() {
+            console.log("onclick")
+            if (clearedTextBox === 0) {
+                text_box.innerHTML = "";
+                clearedTextBox = 1
+            }
+        };
+    }
+    text_box.oninput = function() {
+        localStorage.setItem("input", text_box.innerHTML);
+    }
 });
